8b385721cafd3bf4d6421ac71b7fdf66d24f3960
MostLeVert <axeljalonen@gmail.com>
2023-12-29 18:18:15 -0800

    Add a custom render for marked for heading id's

--- compileMarkdown.mjs
+++ compileMarkdown.mjs
@@ -1,8 +1,19 @@
 import { marked } from 'marked';
 import { readFileSync, writeFileSync } from 'fs';
-
+const renderer = {
+    heading(text, level) {
+      const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+  
+      return `
+              <h${level} id="${escapedText}">
+                ${text}
+              </h${level}>`;
+    }
+  };
+  
+marked.use({ renderer });
+const template = readFileSync('./pages/template.html', 'utf8');
 const markdown = readFileSync('README.md', 'utf8');
 const content = marked.parse(markdown);
-const template = readFileSync('./pages/template.html', 'utf8');
 const output = template.replace('<div id="markdown-content"></div>', `<div id="markdown-content">${content}</div>`);
 writeFileSync('./pages/read-more.html', output);
